6b55ebaf7e2bf8e676d849eedb8526882c3900cc
Jonghakseo <unqocn@gmail.com>
2022-09-26 02:25:01 +0800

    EDIT: reload-on-update function args type

--- src/global.d.ts
+++ src/global.d.ts
@@ -5,12 +5,12 @@ declare namespace chrome {
 }
 
 declare module "virtual:reload-on-update-in-background-script" {
-  export const reloadOnUpdate: () => void;
+  export const reloadOnUpdate: (watchPath: string) => void;
   export default reloadOnUpdate;
 }
 
 declare module "virtual:reload-on-update-in-view" {
-  const refreshOnUpdate: () => void;
+  const refreshOnUpdate: (watchPath: string) => void;
   export default refreshOnUpdate;
 }
 
