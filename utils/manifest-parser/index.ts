525c6018f23b847a98d1f55b1e381ea3bb765990
Jonghakseo <unqocn@gmail.com>
2022-10-08 14:34:10 +0900

    ADD: Manifest Parser

---
+++ utils/manifest-parser/index.ts
@@ -0,0 +1,13 @@
+type Manifest = chrome.runtime.ManifestV3;
+
+class ManifestParser {
+  /** SINGLETON */
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private constructor() {}
+
+  static convertManifestToString(manifest: Manifest): string {
+    return JSON.stringify(manifest, null, 2);
+  }
+}
+
+export default ManifestParser;
