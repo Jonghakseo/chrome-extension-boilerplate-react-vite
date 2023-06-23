e276dc2c1238bf1307ad6ace2736472f2c0fb80e
Jonghakseo <jonghak.seo@creatrip.com>
2023-06-23 17:31:38 +0900

    install: tailwindcss autoprefixer postcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/**/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
