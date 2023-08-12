a09a6b92834608a31e8239b745ea73a741bb3865
codefactor-io <support@codefactor.io>
2023-08-12 09:53:58 +0000

    [CodeFactor] Apply fixes to commit 67eccf0

--- src/shared/hooks/useStorage.tsx
+++ src/shared/hooks/useStorage.tsx
@@ -3,7 +3,7 @@ import { BaseStorage } from "@src/shared/storages/base";
 
 export default function useStorage<
   Storage extends BaseStorage<Data>,
-  Data = Storage extends BaseStorage<infer Data> ? Data : unknown
+  Data = Storage extends BaseStorage<infer Data> ? Data : unknown,
 >(storage: Storage) {
   return useSyncExternalStore<Data>(storage.subscribe, storage.getSnapshot);
 }
