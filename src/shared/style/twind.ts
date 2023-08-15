db9432c0525cdd56b3f922fe6772acb42a43f407
Jonghakseo <unqocn@gmail.com>
2023-08-15 15:43:11 +0900

    add: attachTwindStyle util function

---
+++ src/shared/style/twind.ts
@@ -0,0 +1,13 @@
+import { twind, cssom, observe } from "@twind/core";
+import "construct-style-sheets-polyfill";
+import config from "@root/twind.config";
+
+export function attachTwindStyle<T extends { adoptedStyleSheets: any }>(
+  observedElement: Element,
+  documentOrShadowRoot: T
+) {
+  const sheet = cssom(new CSSStyleSheet());
+  const tw = twind(config, sheet);
+  observe(tw, observedElement);
+  documentOrShadowRoot.adoptedStyleSheets = [sheet.target];
+}
