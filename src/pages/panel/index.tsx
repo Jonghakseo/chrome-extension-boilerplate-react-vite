ff9cc95b752df8efbed3a4acaa2a2ebf9077b1f1
Jonghakseo <unqocn@gmail.com>
2023-08-15 15:43:57 +0900

    fix: use attachTwindStyle into newtab, panel, popup

--- src/pages/panel/index.tsx
+++ src/pages/panel/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import Panel from "@pages/panel/Panel";
 import "@pages/panel/index.css";
 import refreshOnUpdate from "virtual:reload-on-update-in-view";
+import { attachTwindStyle } from "@src/shared/style/twind";
 
 refreshOnUpdate("pages/panel");
 
@@ -11,6 +12,7 @@ function init() {
   if (!appContainer) {
     throw new Error("Can not find #app-container");
   }
+  attachTwindStyle(appContainer, document);
   const root = createRoot(appContainer);
   root.render(<Panel />);
 }
--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "@pages/popup/index.css";
 import Popup from "@pages/popup/Popup";
 import refreshOnUpdate from "virtual:reload-on-update-in-view";
+import { attachTwindStyle } from "@src/shared/style/twind";
 
 refreshOnUpdate("pages/popup");
 
@@ -11,6 +12,7 @@ function init() {
   if (!appContainer) {
     throw new Error("Can not find #app-container");
   }
+  attachTwindStyle(appContainer, document);
   const root = createRoot(appContainer);
   root.render(<Popup />);
 }
