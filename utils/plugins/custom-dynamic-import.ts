b998641b86609be389f86e71d83dbdabdc10146e
Rasul Abu Muhammad Amin <abuhasub@gmail.com>
2023-08-20 21:42:10 +0200

    Support Firefox webextension
    closes #18

--- utils/plugins/custom-dynamic-import.ts
+++ utils/plugins/custom-dynamic-import.ts
@@ -4,6 +4,16 @@ export default function customDynamicImport(): PluginOption {
   return {
     name: "custom-dynamic-import",
     renderDynamicImport() {
+      if (process.env.__FIREFOX__) {
+        return {
+          left: `
+        {
+          const dynamicImport = (path) => import(path);
+          dynamicImport(browser.runtime.getURL('./') + 
+          `,
+          right: ".split('../').join(''))}",
+        };
+      }
       return {
         left: `
         {
