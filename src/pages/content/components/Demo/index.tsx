6fc37f9e2f809772dcb387cfe31e004831a26ab5
Jonghakseo <jonghak.seo@creatrip.com>
2023-08-25 13:44:29 +0900

    add: firefox bug comment

--- src/pages/content/components/Demo/index.tsx
+++ src/pages/content/components/Demo/index.tsx
@@ -16,6 +16,12 @@ rootIntoShadow.id = "shadow-root";
 const shadowRoot = root.attachShadow({ mode: "open" });
 shadowRoot.appendChild(rootIntoShadow);
 
+/**
+ * https://github.com/Jonghakseo/chrome-extension-boilerplate-react-vite/pull/174
+ *
+ * In the firefox environment, the adoptedStyleSheets bug may prevent contentStyle from being applied properly.
+ * Please refer to the PR link above and go back to the contentStyle.css implementation, or raise a PR if you have a better way to improve it.
+ */
 attachTwindStyle(rootIntoShadow, shadowRoot);
 
 createRoot(rootIntoShadow).render(<App />);
