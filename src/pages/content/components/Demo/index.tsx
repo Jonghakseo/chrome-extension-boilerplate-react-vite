eef91c519f89686920df0af439710a5173f5f8d9
JongHak Seo <unqocn@gmail.com>
2023-09-26 18:35:14 +0900

    roll back unnecessary changes

--- src/pages/content/components/Demo/index.tsx
+++ src/pages/content/components/Demo/index.tsx
@@ -1,11 +1,27 @@
 import { createRoot } from "react-dom/client";
 import App from "@src/pages/content/components/Demo/app";
 import refreshOnUpdate from "virtual:reload-on-update-in-view";
+import { attachTwindStyle } from "@src/shared/style/twind";
 
 refreshOnUpdate("pages/content");
 
 const root = document.createElement("div");
 root.id = "chrome-extension-boilerplate-react-vite-content-view-root";
+
 document.body.append(root);
 
-createRoot(root).render(<App />);
+const rootIntoShadow = document.createElement("div");
+rootIntoShadow.id = "shadow-root";
+
+const shadowRoot = root.attachShadow({ mode: "open" });
+shadowRoot.appendChild(rootIntoShadow);
+
+/**
+ * https://github.com/Jonghakseo/chrome-extension-boilerplate-react-vite/pull/174
+ *
+ * In the firefox environment, the adoptedStyleSheets bug may prevent contentStyle from being applied properly.
+ * Please refer to the PR link above and go back to the contentStyle.css implementation, or raise a PR if you have a better way to improve it.
+ */
+attachTwindStyle(rootIntoShadow, shadowRoot);
+
+createRoot(rootIntoShadow).render(<App />);
