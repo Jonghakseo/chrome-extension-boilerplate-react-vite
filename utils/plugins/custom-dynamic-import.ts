e067e8c2d8990fc94ccef775518b3821d74e1c5b
JongHak Seo <unqocn@gmail.com>
2023-10-13 03:05:37 +0900

    issue #157 (#195)

--- utils/plugins/custom-dynamic-import.ts
+++ utils/plugins/custom-dynamic-import.ts
@@ -3,24 +3,31 @@ import type { PluginOption } from "vite";
 export default function customDynamicImport(): PluginOption {
   return {
     name: "custom-dynamic-import",
-    renderDynamicImport() {
-      if (process.env.__FIREFOX__) {
+    renderDynamicImport({ moduleId }) {
+      if (!moduleId.includes("node_modules")) {
+        // ↑ dont modify any import from node_modules
+        if (process.env.__FIREFOX__) {
+          return {
+            left: `
+          {
+            const dynamicImport = (path) => import(path);
+            dynamicImport(browser.runtime.getURL('./') + 
+            `,
+            right: ".split('../').join(''))}",
+          };
+        }
         return {
           left: `
-        {
-          const dynamicImport = (path) => import(path);
-          dynamicImport(browser.runtime.getURL('./') + 
-          `,
-          right: ".split('../').join(''))}",
+          {
+            const dynamicImport = (path) => import(path);
+            dynamicImport(
+            `,
+          right: ")}",
         };
       }
       return {
-        left: `
-        {
-          const dynamicImport = (path) => import(path);
-          dynamicImport(
-          `,
-        right: ")}",
+        left: "import(",
+        right: ")",
       };
     },
   };
--- vite.config.ts
+++ vite.config.ts
@@ -45,6 +45,7 @@ export default defineConfig({
     /** Can slowDown build speed. */
     // sourcemap: isDev,
     minify: isProduction,
+    modulePreload: false,
     reportCompressedSize: isProduction,
     rollupOptions: {
       input: {
