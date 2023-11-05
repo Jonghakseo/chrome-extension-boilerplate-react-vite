1380732985ec0b5ec0e3fbf47ac585d996e89011
Jonghakseo <unqocn@gmail.com>
2023-11-05 18:08:04 +0900

    fix: custom dynamic import plugin

--- utils/plugins/custom-dynamic-import.ts
+++ utils/plugins/custom-dynamic-import.ts
@@ -4,30 +4,15 @@ export default function customDynamicImport(): PluginOption {
   return {
     name: 'custom-dynamic-import',
     renderDynamicImport({ moduleId }) {
-      if (!moduleId.includes('node_modules')) {
-        // ↑ dont modify any import from node_modules
-        if (process.env.__FIREFOX__) {
-          return {
-            left: `
-          {
-            const dynamicImport = (path) => import(path);
-            dynamicImport(browser.runtime.getURL('./') + 
-            `,
-            right: ".split('../').join(''))}",
-          };
-        }
+      if (!moduleId.includes('node_modules') && process.env.__FIREFOX__) {
         return {
-          left: `
-          {
-            const dynamicImport = (path) => import(path);
-            dynamicImport(
-            `,
-          right: ')}',
+          left: `import(browser.runtime.getURL('./') + `,
+          right: ".split('../').join(''));",
         };
       }
       return {
         left: 'import(',
-        right: ')',
+        right: ');',
       };
     },
   };
