3b7f51927e3c6e13f242fb4a97c3efb00012eba0
Jonghakseo <unqocn@gmail.com>
2023-11-11 21:31:21 +0900

    feat: add automatic reconnection logic every second

--- utils/reload/initReloadClient.ts
+++ utils/reload/initReloadClient.ts
@@ -41,6 +41,9 @@ export default function initReloadClient({
     console.log(
       `Reload server disconnected.\nPlease check if the WebSocket server is running properly on ${LOCAL_RELOAD_SOCKET_URL}. This feature detects changes in the code and helps the browser to reload the extension or refresh the current tab.`,
     );
+    setInterval(() => {
+      initReloadClient({ watchPath, onUpdate });
+    }, 1000);
   };
 
   return socket;
