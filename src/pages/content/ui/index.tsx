c65802cf73df8dac30d9961e8f0cf613bdca4da2
Jonghakseo <unqocn@gmail.com>
2023-12-02 22:15:13 +0900

    feat: add a css insertion case to shadow dom

--- src/pages/content/ui/index.tsx
+++ src/pages/content/ui/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from '@pages/content/ui/app';
 import refreshOnUpdate from 'virtual:reload-on-update-in-view';
+import injectedStyle from './injected.css?inline';
 
 refreshOnUpdate('pages/content');
 
@@ -14,6 +15,11 @@ rootIntoShadow.id = 'shadow-root';
 
 const shadowRoot = root.attachShadow({ mode: 'open' });
 shadowRoot.appendChild(rootIntoShadow);
+
+/** Inject styles into shadow dom */
+const styleElement = document.createElement('style');
+styleElement.innerHTML = injectedStyle;
+shadowRoot.appendChild(styleElement);
 
 /**
  * https://github.com/Jonghakseo/chrome-extension-boilerplate-react-vite/pull/174
