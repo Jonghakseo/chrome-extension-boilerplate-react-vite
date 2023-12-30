4b32455d5beabbc9c27c17041d213a781aa61bd5
Leal <151762001+lealahm@users.noreply.github.com>
2023-12-30 10:17:37 +0530

    Update src/pages/injectedContent/ui/app.tsx
    Co-authored-by: Romain Dequidt <romaindequidt@users.noreply.github.com>

--- src/pages/injectedContent/ui/app.tsx
+++ src/pages/injectedContent/ui/app.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 export default function App() {
   useEffect(() => {
-    console.log('content view loaded');
+    console.log('injected content view loaded');
   }, []);
 
   return <div className="">injected content view</div>;
