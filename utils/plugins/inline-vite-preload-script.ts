d0f18200f3031eeecfda1faad1b3cce864226d60
JongHak Seo <unqocn@gmail.com>
2024-01-14 16:17:56 +0900

    feat: add multiple content scripts case

---
+++ utils/plugins/inline-vite-preload-script.ts
@@ -0,0 +1,25 @@
+/**
+ * solution for multiple content scripts
+ * https://github.com/Jonghakseo/chrome-extension-boilerplate-react-vite/issues/177#issuecomment-1784112536
+ */
+export default function inlineVitePreloadScript() {
+  let __vitePreload = '';
+  return {
+    name: 'replace-vite-preload-script-plugin',
+    async renderChunk(code, chunk, options, meta) {
+      if (!/content/.test(chunk.fileName)) {
+        return null;
+      }
+      const chunkName = Object.keys(meta.chunks).find(key => /preload/.test(key));
+      const modules = meta.chunks[chunkName].modules;
+      console.log(modules);
+      if (!__vitePreload) {
+        __vitePreload = modules[Object.keys(modules)[0]].code;
+        __vitePreload = __vitePreload.replaceAll('const ', 'var ');
+      }
+      return {
+        code: __vitePreload + code.split(`\n`).slice(1).join(`\n`),
+      };
+    },
+  };
+}
