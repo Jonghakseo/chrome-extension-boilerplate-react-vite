33654780935de337f325b1316257d53c95f6cbab
Goodacres <jack.goodacre2@hotmail.co.uk>
2024-04-13 11:24:23 +0200

    Buttons

--- extension/src/pages/app/App.tsx
+++ extension/src/pages/app/App.tsx
@@ -4,16 +4,15 @@ import Navbar from '../../components/nav/Navbar';
 
 const Options: React.FC = () => {
   return (
-    <>
+    <div className="min-h-screen bg-background2">
       <Navbar />
-
-      <div className="width-full flex justify-center items-center pt-10">
+      <div className="width-full flex justify-center items-center pt-10 bg-background2">
         <div className="w-1/2 text-left">
           <h1 className="text-xl font-bold pb-6">Blocklock Password Manager</h1>
           <Accordion />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
