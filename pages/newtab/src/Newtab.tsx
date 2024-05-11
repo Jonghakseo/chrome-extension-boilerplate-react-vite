2bccf346336beecc9e07ce8c3f7b95c6a169a757
Jonghakseo <jonghak.seo@creatrip.com>
2024-05-12 01:33:46 +0900

    feat: add tailwindcss into newtab, devtools panel

--- pages/newtab/src/Newtab.tsx
+++ pages/newtab/src/Newtab.tsx
@@ -6,16 +6,13 @@ import {
   withErrorBoundary,
   withSuspense,
 } from '@chrome-extension-boilerplate/shared';
+import { ComponentPropsWithoutRef } from 'react';
 
 const Newtab = () => {
   const theme = useStorageSuspense(exampleThemeStorage);
 
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor: theme === 'light' ? '#eee' : '#222',
-      }}>
+    <div className="App" style={{ backgroundColor: theme === 'light' ? '#eee' : '#222' }}>
       <header className="App-header" style={{ color: theme === 'light' ? '#222' : '#eee' }}>
         <img src={chrome.runtime.getURL('newtab/logo.svg')} className="App-logo" alt="logo" />
         <p>
@@ -30,16 +27,25 @@ const Newtab = () => {
           Learn React
         </a>
         <h6>The color of this paragraph is defined using SASS.</h6>
-        <button
-          style={{
-            backgroundColor: theme === 'light' ? '#eee' : '#222',
-            color: theme === 'light' ? '#222' : '#eee',
-          }}
-          onClick={exampleThemeStorage.toggle}>
-          Toggle theme
-        </button>
+        <ToggleButton>Toggle theme (tailwind)</ToggleButton>
       </header>
     </div>
+  );
+};
+
+const ToggleButton = (props: ComponentPropsWithoutRef<'button'>) => {
+  const theme = useStorageSuspense(exampleThemeStorage);
+  return (
+    <button
+      className={
+        props.className +
+        ' ' +
+        'font-bold mt-4 py-1 px-4 rounded shadow hover:scale-105 ' +
+        (theme === 'light' ? 'bg-white text-black' : 'bg-black text-white')
+      }
+      onClick={exampleThemeStorage.toggle}>
+      {props.children}
+    </button>
   );
 };
 
