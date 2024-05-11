52f8d2c9c3a9391def08a6b3021360fa49edabbe
Jonghakseo <jonghak.seo@creatrip.com>
2024-05-12 01:48:31 +0900

    feat: add tailwindcss into popup, sidepanel, options

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -5,6 +5,7 @@ import {
   withErrorBoundary,
   withSuspense,
 } from '@chrome-extension-boilerplate/shared';
+import { ComponentPropsWithoutRef } from 'react';
 
 const Popup = () => {
   const theme = useStorageSuspense(exampleThemeStorage);
@@ -29,16 +30,25 @@ const Popup = () => {
           style={{ color: theme === 'light' ? '#0281dc' : undefined, marginBottom: '10px' }}>
           Learn React!
         </a>
-        <button
-          style={{
-            backgroundColor: theme === 'light' ? '#eee' : '#222',
-            color: theme === 'light' ? '#222' : '#eee',
-          }}
-          onClick={exampleThemeStorage.toggle}>
-          Toggle theme
-        </button>
+        <ToggleButton>Toggle theme (tailwind)</ToggleButton>
       </header>
     </div>
+  );
+};
+
+const ToggleButton = (props: ComponentPropsWithoutRef<'button'>) => {
+  const theme = useStorageSuspense(exampleThemeStorage);
+  return (
+    <button
+      className={
+        props.className +
+        ' ' +
+        'font-bold mt-4 py-1 px-4 rounded shadow hover:scale-105 ' +
+        (theme === 'light' ? 'bg-white text-black' : 'bg-black text-white')
+      }
+      onClick={exampleThemeStorage.toggle}>
+      {props.children}
+    </button>
   );
 };
 
