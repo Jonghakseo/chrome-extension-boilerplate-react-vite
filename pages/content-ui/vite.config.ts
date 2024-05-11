7ee0874adea74408f98580916924ea8ed7adb7ca
Jonghakseo <jonghak.seo@creatrip.com>
2024-05-12 00:58:20 +0900

    enhance: extract vite, vite plugin for react
    change vite plugin for react to swc

--- pages/content-ui/vite.config.ts
+++ pages/content-ui/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import { makeEntryPointPlugin, watchRebuildPlugin } from '@chrome-extension-boilerplate/hmr';
 import * as child_process from 'child_process';
--- pages/devtools-panel/vite.config.ts
+++ pages/devtools-panel/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import { watchRebuildPlugin } from '@chrome-extension-boilerplate/hmr';
 
