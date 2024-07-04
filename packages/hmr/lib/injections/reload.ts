049e023ec58e59c91dd09c599feedc73e7aea3c5
JongHak Seo <unqocn@gmail.com>
2024-07-04 09:39:55 +0900

    Add chrome types into hmr package (#546)
    Co-authored-by: Jonghakseo <jonghak.seo@creatrip.com>

--- packages/hmr/lib/injections/reload.ts
+++ packages/hmr/lib/injections/reload.ts
@@ -2,8 +2,6 @@ import initClient from '../initClient';
 
 function addReload() {
   const reload = () => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
     chrome.runtime.reload();
   };
 
