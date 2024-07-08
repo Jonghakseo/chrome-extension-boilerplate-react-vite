7b2a59e307790582ed4405b9ff187563816ae122
Patryk Kuniczak <p.kuniczak@gmail.com>
2024-07-08 16:09:32 +0200

    feat: create script for bumping version of all package.json

---
+++ update_version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Usage: ./update_version.sh <new_version>
+
+find . -name 'package.json' -not -path '*/node_modules/*' -exec bash -c '
+  # Parse the version from package.json
+  current_version=$(grep -o "\"version\": \"[^\"]*" "$0" | cut -d"\"" -f4)
+
+  # Update the version
+  sed -i "s/$current_version/'$1'/" "$0"
+'  {} \;
+
+echo "Updated versions to $1"
