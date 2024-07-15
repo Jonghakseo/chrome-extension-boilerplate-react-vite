7ef613c104fccbdeb3da279fb37fc1fa67788492
Jonghakseo <jonghak.seo@creatrip.com>
2024-07-15 18:53:24 +0900

    feat: make withPageConfig hoc

---
+++ packages/vite-config/withPageConfig.mjs
@@ -0,0 +1,33 @@
+import { defineConfig, UserConfig } from 'vite';
+import { watchRebuildPlugin } from '@chrome-extension-boilerplate/hmr';
+import react from '@vitejs/plugin-react-swc';
+import deepmerge from 'deepmerge';
+import { isDev, isProduction } from './env.mjs';
+
+/**
+ * @param {UserConfig} config
+ * @returns {UserConfig}
+ */
+export function withPageConfig(config) {
+  return defineConfig(
+    deepmerge(
+      {
+        base: '',
+        plugins: [react(), isDev && watchRebuildPlugin({ refresh: true })],
+        build: {
+          sourcemap: isDev,
+          minify: isProduction,
+          reportCompressedSize: isProduction,
+          emptyOutDir: true,
+          rollupOptions: {
+            external: ['chrome'],
+          },
+        },
+        define: {
+          'process.env.NODE_ENV': isDev ? `"development"` : `"production"`,
+        },
+      },
+      config,
+    ),
+  );
+}
--- pages/options/vite.config.mts
+++ pages/options/vite.config.mts
@@ -1,11 +1,10 @@
 import { resolve } from 'path';
-import baseConfig from '@chrome-extension-boilerplate/vite-base-config/vite.base.config.mjs';
-import { mergeViteConfigs } from '@chrome-extension-boilerplate/utils/dist/lib/configMerger';
+import { withPageConfig } from '@chrome-extension-boilerplate/vite-config';
 
 const rootDir = resolve(__dirname);
 const srcDir = resolve(rootDir, 'src');
 
-export default mergeViteConfigs(baseConfig, {
+export default withPageConfig({
   resolve: {
     alias: {
       '@src': srcDir,
@@ -14,5 +13,5 @@ export default mergeViteConfigs(baseConfig, {
   publicDir: resolve(rootDir, 'public'),
   build: {
     outDir: resolve(rootDir, '..', '..', 'dist', 'options'),
-  }
+  },
 });
