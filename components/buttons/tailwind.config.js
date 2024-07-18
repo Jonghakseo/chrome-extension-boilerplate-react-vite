b00b3cbe6fdb8bbb82f7bf1b9fb49bc95c8534d9
Jonghakseo <jonghak.seo@creatrip.com>
2024-07-18 22:04:31 +0900

    feat: delete components/buttons

--- components/buttons/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const baseConfig = require('@chrome-extension-boilerplate/tailwindcss-config');
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  ...baseConfig,
-  content: ['./index.html', './lib/**/*.{js,ts,jsx,tsx}'],
-};
