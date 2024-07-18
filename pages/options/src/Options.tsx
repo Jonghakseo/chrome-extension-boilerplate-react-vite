d14d06d294aed52de89a4919e3c1d8c7785e772f
JongHak Seo <unqocn@gmail.com>
2024-07-19 00:26:15 +0900

    feat: use ui package on options

--- pages/options/src/Options.tsx
+++ pages/options/src/Options.tsx
@@ -1,11 +1,10 @@
 import '@src/Options.css';
 import { useStorageSuspense, withErrorBoundary, withSuspense } from '@chrome-extension-boilerplate/shared';
 import { exampleThemeStorage } from '@chrome-extension-boilerplate/storage';
-import { ToggleButton } from '@chrome-extension-boilerplate/buttons';
+import { Button } from '@chrome-extension-boilerplate/ui';
 
 const Options = () => {
   const theme = useStorageSuspense(exampleThemeStorage);
-
   return (
     <div
       className="App-container"
@@ -13,9 +12,11 @@ const Options = () => {
         backgroundColor: theme === 'light' ? '#eee' : '#222',
       }}>
       <img src={chrome.runtime.getURL('options/logo.svg')} className="App-logo" alt="logo" />
-      <span style={{ color: theme === 'light' ? '#0281dc' : undefined, marginBottom: '10px' }}>Options</span>
-      Edit <code>pages/options/src/Options.tsx</code> and save to reload.
-      <ToggleButton>Toggle theme</ToggleButton>
+      <span style={{ color: theme === 'light' ? '#0281dc' : '', marginBottom: '10px' }}>Options</span>
+      Edit <code className={'bg-rose-50'}>pages/options/src/Options.tsx</code> and save to reload.
+      <Button onClick={exampleThemeStorage.toggle} theme={theme}>
+        Toggle Theme
+      </Button>
     </div>
   );
 };
