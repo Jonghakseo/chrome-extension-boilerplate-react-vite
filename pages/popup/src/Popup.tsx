4cfdcd994d594bcb7fa77000f82d947f4d6d35ca
Patryk Kuniczak <p.kuniczak@gmail.com>
2024-07-20 16:50:36 +0200

    feat: create little border to toggle button

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -46,7 +46,7 @@ const ToggleButton = (props: ComponentPropsWithoutRef<'button'>) => {
         props.className +
         ' ' +
         'font-bold mt-4 py-1 px-4 rounded shadow hover:scale-105 ' +
-        (theme === 'light' ? 'bg-white text-black' : 'bg-black text-white')
+        (theme === 'light' ? 'bg-white text-black shadow-black' : 'bg-black text-white')
       }
       onClick={exampleThemeStorage.toggle}>
       {props.children}
