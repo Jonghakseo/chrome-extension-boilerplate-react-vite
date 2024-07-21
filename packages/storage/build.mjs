3ed01898004f720f2b2b57c7eda39e71b96647ef
JongHak Seo <unqocn@gmail.com>
2024-07-22 00:47:33 +0900

    fix: chrome reference error when use storage in ui package (#599)
    Co-authored-by: Jonghakseo <jonghak.seo@creatrip.com>

---
+++ packages/storage/build.mjs
@@ -0,0 +1,15 @@
+import * as esbuild from 'esbuild';
+
+/**
+ * @type { import("esbuild").BuildOptions }
+ */
+const buildOptions = {
+  entryPoints: ['./index.ts', './lib/**/*.ts', './lib/**/*.tsx'],
+  tsconfig: './tsconfig.json',
+  bundle: false,
+  target: 'es6',
+  outdir: './dist',
+  sourcemap: true,
+};
+
+await esbuild.build(buildOptions);
