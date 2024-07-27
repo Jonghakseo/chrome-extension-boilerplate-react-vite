481a2a88704f21543f57c856d3a7f369ff10f6f3
JongHak Seo <unqocn@gmail.com>
2024-07-27 23:38:34 +0900

    feat: Rename import alias for i18n module
    Renamed the import alias `mock` to `t_dev_or_prod` for the i18n module. This change ensures a clearer and more accurate representation of the import's purpose in both development and production environments.

--- packages/i18n/index.ts
+++ packages/i18n/index.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import { t as mock } from './lib/i18n';
+import { t as t_dev_or_prod } from './lib/i18n';
 import { t as t_dev } from 'lib/i18n-dev';
 
-export const t = mock as unknown as typeof t_dev;
+export const t = t_dev_or_prod as unknown as typeof t_dev;
