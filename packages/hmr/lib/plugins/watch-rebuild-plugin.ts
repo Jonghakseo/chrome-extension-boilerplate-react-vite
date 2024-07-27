491fda23633d89a4b2399cd98b2324ccbdd727d5
Jonghakseo <jonghak.seo@creatrip.com>
2024-07-27 21:48:47 +0900

    Revert "refactor: Extract HMR code retrieval to a separate module"
    This reverts commit 04cc2fbed1f496d96b25d454180859cd9f35b2d2.

--- packages/hmr/lib/plugins/watch-rebuild-plugin.ts
+++ packages/hmr/lib/plugins/watch-rebuild-plugin.ts
@@ -1,8 +1,9 @@
 import type { PluginOption } from 'vite';
 import { type WebSocket } from 'ws';
 import MessageInterpreter from '../interpreter';
+import { readFileSync } from 'fs';
+import path from 'path';
 import { initializeWSS } from '../initializeWSS';
-import { getTranspiledHMRCode } from '../getTranspiledHMRCode';
 
 type PluginConfig = {
   onStart?: () => void;
@@ -10,11 +11,17 @@ type PluginConfig = {
   refresh?: boolean;
 };
 
+const injectionsPath = path.resolve(__dirname, '..', '..', '..', 'build', 'injections');
+
+const refreshCode = readFileSync(path.resolve(injectionsPath, 'refresh.js'), 'utf-8');
+const reloadCode = readFileSync(path.resolve(injectionsPath, 'reload.js'), 'utf-8');
+
 export function watchRebuildPlugin(config: PluginConfig): PluginOption {
   let ws: WebSocket | null = null;
   const id = Math.random().toString(36);
-  const { refresh = false, reload = false } = config;
-  const hmrCode = getTranspiledHMRCode({ refresh, reload });
+  const { refresh, reload } = config;
+
+  const hmrCode = (refresh ? refreshCode : '') + (reload ? reloadCode : '');
 
   return {
     name: 'watch-rebuild',
