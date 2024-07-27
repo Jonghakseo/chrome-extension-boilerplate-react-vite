dd323cbc45a428d4321cfced5c5ebf14aa4ce573
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2024-07-27 09:10:41 +0200

    hotfix/update-version script path (#593)
    Co-authored-by: JongHak Seo <unqocn@gmail.com>

--- update_version.sh
+++ update_version.sh
@@ -11,7 +11,7 @@ if [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     perl -i -pe"s/$current_version/'$1'/" "$0"
   '  {} \;
 
-  echo "Updated versions to $1"
+  echo "Updated versions to $1";
 else
-    echo "Version format <$1> isn't correct, proper format is <0.0.0>"
+  echo "Version format <$1> isn't correct, proper format is <0.0.0>";
 fi
