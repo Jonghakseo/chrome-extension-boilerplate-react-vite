8abc4f6f07aba0b2912a68f5339b9d33e98b7350
JongHak Seo <unqocn@gmail.com>
2024-08-04 23:40:52 +0900

    feat: Add placeholder replacement logic in translation function

--- packages/i18n/lib/i18n-dev.ts
+++ packages/i18n/lib/i18n-dev.ts
@@ -1,14 +1,32 @@
 import { DevLocale, MessageKey } from './type';
 import { defaultLocale, getMessageFromLocale } from './getMessageFromLocale';
 
-function translate(key: MessageKey, substitutions?: string | string[]) {
-  const message = getMessageFromLocale(t.devLocale)[key].message;
+type I18nValue = {
+  message: string;
+  placeholders?: Record<string, { content?: string; exampe?: string }>;
+};
 
+function translate(key: MessageKey, substitutions?: string | string[]) {
+  const value = getMessageFromLocale(t.devLocale)[key] as I18nValue;
+  let message = value.message;
+  /**
+   * This is a placeholder replacement logic. But it's not perfect.
+   * It just imitates the behavior of the Chrome extension i18n API.
+   * Please check the official document for more information And double-check the behavior on production build.
+   *
+   * @url https://developer.chrome.com/docs/extensions/how-to/ui/localization-message-formats#placeholders
+   */
+  if (value.placeholders) {
+    Object.entries(value.placeholders).forEach(([key, { content }]) => {
+      if (!content) {
+        return;
+      }
+      message = message.replace(new RegExp(`\\$${key}\\$`, 'gi'), content);
+    });
+  }
   if (!substitutions) {
     return message;
   }
-
-  // TODO: Implement all substitution logic (placeholders fields)
   if (Array.isArray(substitutions)) {
     return substitutions.reduce((acc, cur, idx) => acc.replace(`$${idx + 1}`, cur), message);
   }
