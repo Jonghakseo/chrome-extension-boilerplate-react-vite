25b02be0ce0f5f1a5cf94580ddae4ca4ff1cd590
Patryk Kuniczak <p.kuniczak@gmail.com>
2024-08-06 18:04:14 +0200

    fix: remove unnecessary blank quote

--- pages/devtools-panel/src/Panel.tsx
+++ pages/devtools-panel/src/Panel.tsx
@@ -27,7 +27,6 @@ const ToggleButton = (props: ComponentPropsWithoutRef<'button'>) => {
     <button
       className={
         props.className +
-        ' ' +
         'font-bold mt-4 py-1 px-4 rounded shadow hover:scale-105 ' +
         (theme === 'light' ? 'bg-white text-black' : 'bg-black text-white')
       }
