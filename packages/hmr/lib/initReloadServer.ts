e48cd2109eb5c11b08d5f22580fb2abc77e28f6b
Patryk Kuniczak <p.kuniczak@gmail.com>
2024-08-14 09:44:33 +0200

    fix: add clearing set on wss close and update message types

--- packages/hmr/lib/initReloadServer.ts
+++ packages/hmr/lib/initReloadServer.ts
@@ -4,46 +4,48 @@ import type { WebSocket } from 'ws';
 import { WebSocketServer } from 'ws';
 import { LOCAL_RELOAD_SOCKET_PORT, LOCAL_RELOAD_SOCKET_URL } from './constant';
 import MessageInterpreter from './interpreter';
+import { BUILD_COMPLETE, DO_UPDATE, DONE_UPDATE } from './interpreter/types';
 
 const clientsThatNeedToUpdate: Set<WebSocket> = new Set();
 
 function initReloadServer() {
-  try {
-    const wss = new WebSocketServer({ port: LOCAL_RELOAD_SOCKET_PORT });
+  const wss = new WebSocketServer({ port: LOCAL_RELOAD_SOCKET_PORT });
 
-    wss.on('listening', () => console.log(`[HMR] Server listening at ${LOCAL_RELOAD_SOCKET_URL}`));
+  wss.on('listening', () => {
+    console.log(`[HMR] Server listening at ${LOCAL_RELOAD_SOCKET_URL}`);
+  });
 
-    wss.on('connection', ws => {
-      clientsThatNeedToUpdate.add(ws);
+  wss.on('connection', ws => {
+    clientsThatNeedToUpdate.add(ws);
 
-      ws.addEventListener('close', () => clientsThatNeedToUpdate.delete(ws));
-      ws.addEventListener('message', event => {
-        if (typeof event.data !== 'string') return;
-
-        const message = MessageInterpreter.receive(event.data);
-
-        if (message.type === 'done_update') {
-          ws.close();
-        }
-        if (message.type === 'build_complete') {
-          clientsThatNeedToUpdate.forEach((ws: WebSocket) =>
-            ws.send(MessageInterpreter.send({ type: 'do_update', id: message.id })),
-          );
-        }
-      });
+    ws.addEventListener('close', () => {
+      clientsThatNeedToUpdate.delete(ws);
     });
 
-    ping();
-  } catch {
+    ws.addEventListener('message', event => {
+      if (typeof event.data !== 'string') return;
+
+      const message = MessageInterpreter.receive(event.data);
+
+      if (message.type === DONE_UPDATE) {
+        ws.close();
+      }
+
+      if (message.type === BUILD_COMPLETE) {
+        clientsThatNeedToUpdate.forEach((ws: WebSocket) =>
+          ws.send(MessageInterpreter.send({ type: DO_UPDATE, id: message.id })),
+        );
+      }
+    });
+  });
+
+  wss.on('close', () => {
+    clientsThatNeedToUpdate.clear();
+  });
+
+  wss.on('error', () => {
     console.error(`[HMR] Failed to start server at ${LOCAL_RELOAD_SOCKET_URL}`);
-  }
+  });
 }
 
 initReloadServer();
-
-function ping() {
-  clientsThatNeedToUpdate.forEach(ws => ws.send(MessageInterpreter.send({ type: 'ping' })));
-  setTimeout(() => {
-    ping();
-  }, 15_000);
-}
