9d986a1fabbae5969d994716bd739e244a868c2c
Patryk Kuniczak <p.kuniczak@gmail.com>
2024-08-14 09:37:06 +0200

    fix: simplify initClient and create message type as const

--- packages/hmr/lib/initClient.ts
+++ packages/hmr/lib/initClient.ts
@@ -1,39 +1,19 @@
 import { LOCAL_RELOAD_SOCKET_URL } from './constant';
 import MessageInterpreter from './interpreter';
+import { DO_UPDATE, DONE_UPDATE } from './interpreter/types';
 
-export default function initReloadClient({ id, onUpdate }: { id: string; onUpdate: () => void }) {
-  let ws: WebSocket | null = null;
-  try {
-    ws = new WebSocket(LOCAL_RELOAD_SOCKET_URL);
-    ws.onopen = () => {
-      ws?.addEventListener('message', event => {
-        const message = MessageInterpreter.receive(String(event.data));
-        if (message.type === 'ping') {
-          console.log('[HMR] Client OK');
-        }
-        if (message.type === 'do_update' && message.id === id) {
-          sendUpdateCompleteMessage();
-          onUpdate();
-          return;
-        }
-      });
-    };
+export default function initWsClient({ id, onUpdate }: { id: string; onUpdate: () => void }) {
+  const ws = new WebSocket(LOCAL_RELOAD_SOCKET_URL);
 
-    ws.onclose = () => {
-      console.log(
-        `Reload server disconnected.\nPlease check if the WebSocket server is running properly on ${LOCAL_RELOAD_SOCKET_URL}. This feature detects changes in the code and helps the browser to reload the extension or refresh the current tab.`,
-      );
-      setTimeout(() => {
-        initReloadClient({ onUpdate, id });
-      }, 1000);
-    };
-  } catch (e) {
-    setTimeout(() => {
-      initReloadClient({ onUpdate, id });
-    }, 1000);
-  }
+  ws.onopen = () => {
+    ws.addEventListener('message', event => {
+      const message = MessageInterpreter.receive(String(event.data));
 
-  function sendUpdateCompleteMessage() {
-    ws?.send(MessageInterpreter.send({ type: 'done_update' }));
-  }
+      if (message.type === DO_UPDATE && message.id === id) {
+        ws.send(MessageInterpreter.send({ type: DONE_UPDATE }));
+        onUpdate();
+        return;
+      }
+    });
+  };
 }
--- packages/hmr/lib/interpreter/types.ts
+++ packages/hmr/lib/interpreter/types.ts
@@ -1,11 +1,17 @@
+export const DO_UPDATE = 'do_update';
+export const DONE_UPDATE = 'done_update';
+export const BUILD_COMPLETE = 'build_complete';
+export const ERROR = 'error';
+
 type UpdateRequestMessage = {
-  type: 'do_update';
+  type: typeof DO_UPDATE;
   id: string;
 };
-type UpdateCompleteMessage = { type: 'done_update' };
-type PingMessage = { type: 'ping' };
-type BuildCompletionMessage = { type: 'build_complete'; id: string };
+
+type UpdateCompleteMessage = { type: typeof DONE_UPDATE };
+type ErrorMessage = { type: typeof ERROR };
+type BuildCompletionMessage = { type: typeof BUILD_COMPLETE; id: string };
 
 export type SerializedMessage = string;
 
-export type WebSocketMessage = UpdateCompleteMessage | UpdateRequestMessage | BuildCompletionMessage | PingMessage;
+export type WebSocketMessage = UpdateCompleteMessage | UpdateRequestMessage | BuildCompletionMessage | ErrorMessage;
