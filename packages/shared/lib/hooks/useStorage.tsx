2a3aec57056b8ec5c1ed4b80151bcbcf422584fa
Patryk Kuniczak <p.kuniczak@gmail.com>
2024-08-15 11:04:21 +0200

    fix: remove redundant useStorage hook

--- packages/shared/lib/hooks/useStorage.tsx
+++ packages/shared/lib/hooks/useStorage.tsx
@@ -48,19 +48,3 @@ function wrapPromise<R>(promise: Promise<R>) {
     },
   };
 }
-
-export function useStorage<
-  Storage extends BaseStorage<Data>,
-  Data = Storage extends BaseStorage<infer Data> ? Data : unknown,
->(storage: Storage) {
-  const _data = useSyncExternalStore<Data | null>(storage.subscribe, storage.getSnapshot);
-
-  if (!storageMap.has(storage)) {
-    storageMap.set(storage, wrapPromise(storage.get()));
-  }
-  if (_data !== null) {
-    storageMap.set(storage, { read: () => _data });
-  }
-
-  return (_data ?? storageMap.get(storage)!.read()) as Exclude<Data, PromiseLike<unknown>>;
-}
