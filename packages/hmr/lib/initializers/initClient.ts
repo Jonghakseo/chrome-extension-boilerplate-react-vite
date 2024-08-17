6b95f809af80c607b9814fce35082d4d1d4a37f2
Patryk Kuniczak <p.kuniczak@gmail.com>
2024-08-17 12:50:19 +0200

    fix: remove wrong import after rebase

--- packages/hmr/lib/initializers/initClient.ts
+++ packages/hmr/lib/initializers/initClient.ts
@@ -1,6 +1,5 @@
 import { DO_UPDATE, DONE_UPDATE, LOCAL_RELOAD_SOCKET_URL } from '../constant';
 import MessageInterpreter from '../interpreter';
-import { DO_UPDATE, DONE_UPDATE } from '../types';
 
 export default function initClient({ id, onUpdate }: { id: string; onUpdate: () => void }) {
   const ws = new WebSocket(LOCAL_RELOAD_SOCKET_URL);
