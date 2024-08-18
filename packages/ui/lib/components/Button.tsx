a85899724bd710c90e071033ae1ceaa7b763b87a
Jonghakseo <unqocn@gmail.com>
2024-08-19 00:02:03 +0900

    feat: edit example Button style

--- packages/ui/lib/components/Button.tsx
+++ packages/ui/lib/components/Button.tsx
@@ -10,7 +10,7 @@ export function Button({ theme, className, children, ...props }: ButtonProps) {
     <button
       className={cn(
         className,
-        'mt-4 py-1 px-4 rounded shadow hover:scale-105',
+        'py-1 px-4 rounded shadow hover:scale-105',
         theme === 'light' ? 'bg-white text-black' : 'bg-black text-white',
       )}
       {...props}>
