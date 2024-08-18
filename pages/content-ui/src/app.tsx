0b136520eb24e2ba725fe6482f548e15c11a4d59
Jonghakseo <unqocn@gmail.com>
2024-08-19 00:02:47 +0900

    feat: add content-ui examples

--- pages/content-ui/src/app.tsx
+++ pages/content-ui/src/app.tsx
@@ -1,13 +1,23 @@
 import { useEffect } from 'react';
+import { Button } from '@extension/ui';
+import { useStorageSuspense } from '@extension/shared';
+import { exampleThemeStorage } from '@extension/storage';
 
 export default function App() {
+  const theme = useStorageSuspense(exampleThemeStorage);
+
   useEffect(() => {
     console.log('content ui loaded');
   }, []);
 
   return (
-    <div className={'flex gap-1 text-blue-500'}>
-      Edit <strong className={'text-blue-700'}>pages/content-ui/src/app.tsx</strong> and save to reload.
+    <div className="flex items-center justify-between gap-2 bg-blue-100 rounded py-1 px-2">
+      <div className="flex gap-1 text-blue-500">
+        Edit <strong className="text-blue-700">pages/content-ui/src/app.tsx</strong> and save to reload.
+      </div>
+      <Button theme={theme} onClick={exampleThemeStorage.toggle}>
+        Toggle Theme
+      </Button>
     </div>
   );
 }
--- pages/content-ui/tailwind.config.js
+++ pages/content-ui/tailwind.config.js
@@ -1,7 +1,8 @@
 const baseConfig = require('@extension/tailwindcss-config');
+const { withUI } = require('@extension/ui');
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+module.exports = withUI({
   ...baseConfig,
   content: ['src/**/*.{ts,tsx}'],
-};
+});
