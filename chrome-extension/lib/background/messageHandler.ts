25baad33b5602c4612dc7d83dad43e4eb3cffc79
Jonghakseo <jonghak.seo@creatrip.com>
2024-09-01 21:17:11 +0900

    feat: add async example

--- chrome-extension/lib/background/messageHandler.ts
+++ chrome-extension/lib/background/messageHandler.ts
@@ -1,5 +1,11 @@
 import { messaging } from '@extension/messaging';
 
 export function addMessageHandler() {
-  messaging.on('foo', async ({ bar }) => `Hello, ${bar}!`);
+  messaging.on(
+    'foo',
+    async ({ bar }) =>
+      new Promise(resolve => {
+        setTimeout(() => resolve(`Hello, ${bar}!`), 1000);
+      }),
+  );
 }
