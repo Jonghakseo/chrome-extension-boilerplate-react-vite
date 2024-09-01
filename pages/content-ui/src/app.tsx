2dfaf547c057cd0b40e6357d1e2124fa3236cbd0
Jonghakseo <jonghak.seo@creatrip.com>
2024-09-01 21:10:41 +0900

    feat: rollback content-ui changes

--- pages/content-ui/src/app.tsx
+++ pages/content-ui/src/app.tsx
@@ -1,7 +1,12 @@
 import { useEffect } from 'react';
 import { messaging } from '@extension/messaging';
+import { Button } from '@extension/ui';
+import { useStorageSuspense } from '@extension/shared';
+import { exampleThemeStorage } from '@extension/storage';
 
 export default function App() {
+  const theme = useStorageSuspense(exampleThemeStorage);
+
   useEffect(() => {
     console.log('content ui loaded');
     messaging.send('foo', { bar: 'baz' }).then(response => {
@@ -10,10 +15,13 @@ export default function App() {
   }, []);
 
   return (
-    <div className="flex items-center justify-between bg-blue-100 rounded py-1 px-2">
+    <div className="flex items-center justify-between gap-2 bg-blue-100 rounded py-1 px-2">
       <div className="flex gap-1 text-blue-500">
         Edit <strong className="text-blue-700">pages/content-ui/src/app.tsx</strong> and save to reload.
       </div>
+      <Button theme={theme} onClick={exampleThemeStorage.toggle}>
+        Toggle Theme
+      </Button>
     </div>
   );
 }
