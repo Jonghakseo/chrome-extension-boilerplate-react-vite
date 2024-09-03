a3413822212be4177257f198b1ad83d02cd85ae6
Paul Ebose <paulebose@gmail.com>
2024-09-03 19:56:57 +0100

    migrate to eslint v9 flat config
    - replace `@typescript-eslint/*` with `typescript-eslint`
    - install helper eslint v9 packages
    - update `eslint-plugin-import` version to support flat config
    - update `eslint-plugin-react-hooks` version to support eslint v9 api

---
+++ eslint.config.mjs
@@ -0,0 +1,62 @@
+// @ts-check
+
+import { fixupConfigRules } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import reactPlugin from 'eslint-plugin-react';
+import globals from 'globals';
+import ts from 'typescript-eslint';
+
+export default ts.config(
+  // Ignore patterns (.eslintignore)
+  {
+    ignores: ['dist', 'node_modules', 'tailwind.config.ts', 'watch.js'],
+  },
+
+  // Files to scan
+  { files: ['**/*.{js,ts,tsx}'] },
+
+  // Shared configs
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+  jsxA11y.flatConfigs.recommended,
+  eslintPluginPrettierRecommended,
+
+  // Add compatibility
+  // @ts-expect-error - mismatched typings
+  ...fixupConfigRules(new FlatCompat().extends('plugin:react-hooks/recommended')),
+  // ...fixupConfigRules(new FlatCompat().extends('plugin:react-hooks/recommended', 'plugin:import/recommended')),
+
+  // General
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+        ...globals.node,
+        chrome: 'readonly',
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'import/no-unresolved': 'off',
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+    },
+  },
+);
