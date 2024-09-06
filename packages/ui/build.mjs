8e056053cf10792b4433c83f702cad45ea1b807e
Paul Ebose <paulebose@gmail.com>
2024-09-06 21:33:27 +0100

    Merge branch 'main' of https://github.com/Jonghakseo/chrome-extension-boilerplate-react-vite into migrate-to-eslint-v9

--- packages/ui/build.mjs
+++ packages/ui/build.mjs
@@ -1,5 +1,4 @@
 import fs from 'node:fs';
-import { replaceTscAliasPaths } from 'tsc-alias';
 import { resolve } from 'node:path';
 import esbuild from 'esbuild';
 
@@ -16,17 +15,4 @@ const buildOptions = {
 };
 
 await esbuild.build(buildOptions);
-
-/**
- * Post build paths resolve since ESBuild only natively
- * support paths resolution for bundling scenario
- * @url https://github.com/evanw/esbuild/issues/394#issuecomment-1537247216
- */
-await replaceTscAliasPaths({
-  configFile: 'tsconfig.json',
-  watch: false,
-  outDir: 'dist',
-  declarationDir: 'dist',
-});
-
 fs.copyFileSync(resolve('lib', 'global.css'), resolve('dist', 'global.css'));
