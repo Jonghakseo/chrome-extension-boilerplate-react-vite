f6c0777e1f9102974452eaf4fd7b41d023e7d485
RutamBhagat <rutambhagat@gmail.com>
2024-09-15 02:43:43 +0530

    feat(eslint): add eslint-plugin-tailwindcss

--- pages/content-ui/src/App.tsx
+++ pages/content-ui/src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
   }, []);
 
   return (
-    <div className="flex items-center justify-between gap-2 bg-blue-100 rounded py-1 px-2">
+    <div className="flex items-center justify-between gap-2 rounded bg-blue-100 px-2 py-1">
       <div className="flex gap-1 text-blue-500">
         Edit <strong className="text-blue-700">pages/content-ui/src/app.tsx</strong> and save to reload.
       </div>
