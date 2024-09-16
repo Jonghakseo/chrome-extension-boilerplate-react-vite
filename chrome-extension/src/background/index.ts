f17cb502a0dc9fd453bd05bdf60cb9119aeb4eaa
Rasul Abu Muhammad Amin <abuhasub@gmail.com>
2024-09-16 22:02:39 +0200

    Example how to use webext-bridge messaging

--- chrome-extension/src/background/index.ts
+++ chrome-extension/src/background/index.ts
@@ -1,5 +1,7 @@
 import 'webextension-polyfill';
 import { exampleThemeStorage } from '@extension/storage';
+import { onMessage } from 'webext-bridge/background';
+import type { HandshakeResponse } from 'webext-bridge';
 
 exampleThemeStorage.get().then(theme => {
   console.log('theme', theme);
@@ -7,3 +9,8 @@ exampleThemeStorage.get().then(theme => {
 
 console.log('background loaded');
 console.log("Edit 'chrome-extension/src/background/index.ts' and save to reload.");
+
+onMessage('Handshake', async (message): Promise<HandshakeResponse> => {
+  console.log('Received message', message);
+  return 'Yes';
+});
