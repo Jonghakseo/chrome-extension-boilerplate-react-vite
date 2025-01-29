7ea92e01675680ff400f905611ebf665e48c33f2
tr4cks <genot.clement@gmail.com>
2025-01-29 13:30:43 +0100

    Fix: theme toggle each time content script is loaded (#835)

---
+++ pages/content/src/sampleFunction.ts
@@ -0,0 +1,3 @@
+export function sampleFunction() {
+  console.log('content script - sampleFunction() called from another module');
+}
--- pages/content/src/toggleTheme.ts
+++
@@ -1,7 +0,0 @@
-import { exampleThemeStorage } from '@extension/storage';
-
-export async function toggleTheme() {
-  console.log('initial theme:', await exampleThemeStorage.get());
-  await exampleThemeStorage.toggle();
-  console.log('toggled theme:', await exampleThemeStorage.get());
-}
