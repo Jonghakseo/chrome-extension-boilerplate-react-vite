0c8eaf1a22fe9b69097f048a2601cae198502ca0
PatrykKuniczak <p.kuniczak@gmail.com>
2025-01-30 14:18:27 +0100

    clean: rename END_SIGNALS const to TERMINATE_SIGNALS

--- packages/hmr/lib/initializers/cleanDistBackgroundOnProcessEnd.ts
+++ packages/hmr/lib/initializers/cleanDistBackgroundOnProcessEnd.ts
@@ -1,10 +1,10 @@
 import { resolve } from 'node:path';
 import { readFileSync, writeFileSync } from 'node:fs';
-import { END_SIGNALS } from '../consts.js';
+import { TERMINATE_SIGNALS } from '../consts.js';
 
 // THIS FUNCTION CHANGE IS_DEV VAR IN BUNDLED BACKGROUND FILE TO FALSE FOR AVOID WS CONNECTING ISSUE: https://github.com/Jonghakseo/chrome-extension-boilerplate-react-vite/issues/477
 (() => {
-  for (const signal of END_SIGNALS) {
+  for (const signal of TERMINATE_SIGNALS) {
     process.on(signal, () => {
       const backgroundFilePath = resolve(import.meta.dirname, '../../../../../dist/background.js');
 
