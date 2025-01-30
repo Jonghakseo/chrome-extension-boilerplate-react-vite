fc7b25ab11039ad0fc40d34d3521b98667783967
PatrykKuniczak <p.kuniczak@gmail.com>
2025-01-30 14:08:45 +0100

    fix: create checking if IS_DEV before init ws client

--- packages/hmr/lib/initializers/initClient.ts
+++ packages/hmr/lib/initializers/initClient.ts
@@ -1,17 +1,20 @@
 import { DO_UPDATE, DONE_UPDATE, LOCAL_RELOAD_SOCKET_URL } from '../consts.js';
 import MessageInterpreter from '../interpreter/index.js';
+import { IS_DEV } from '@extension/env';
 
 export default ({ id, onUpdate }: { id: string; onUpdate: () => void }) => {
-  const ws = new WebSocket(LOCAL_RELOAD_SOCKET_URL);
+  if (IS_DEV) {
+    const ws = new WebSocket(LOCAL_RELOAD_SOCKET_URL);
 
-  ws.onopen = () => {
-    ws.addEventListener('message', event => {
-      const message = MessageInterpreter.receive(String(event.data));
+    ws.onopen = () => {
+      ws.addEventListener('message', event => {
+        const message = MessageInterpreter.receive(String(event.data));
 
-      if (message.type === DO_UPDATE && message.id === id) {
-        onUpdate();
-        ws.send(MessageInterpreter.send({ type: DONE_UPDATE }));
-      }
-    });
-  };
+        if (message.type === DO_UPDATE && message.id === id) {
+          onUpdate();
+          ws.send(MessageInterpreter.send({ type: DONE_UPDATE }));
+        }
+      });
+    };
+  }
 };
