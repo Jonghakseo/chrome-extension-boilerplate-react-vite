ebda9461467425f736e513a2535de23b07f2c89b
Jonghakseo <jonghak.seo@creatrip.com>
2025-01-31 10:53:04 +0900

    chore: delete base button component

--- packages/ui/lib/components/ToggleButton.tsx
+++ packages/ui/lib/components/ToggleButton.tsx
@@ -1,20 +1,25 @@
-import type { ButtonProps } from './Button';
-import { Button } from './Button';
 import { exampleThemeStorage } from '@extension/storage';
 import { useStorage } from '@extension/shared';
-import { cn } from '../utils';
+import type { ComponentPropsWithoutRef } from 'react';
+import { cn } from '@/lib/utils';
 
-type ToggleButtonProps = ButtonProps;
+type ToggleButtonProps = ComponentPropsWithoutRef<'button'>;
 
 export const ToggleButton = ({ className, children, ...props }: ToggleButtonProps) => {
   const theme = useStorage(exampleThemeStorage);
 
   return (
-    <Button
-      className={cn(className, theme === 'light' ? 'border-black' : 'border-white', 'mt-4 border-2 font-bold')}
+    <button
+      className={cn(
+        className,
+        'py-1 px-4 rounded shadow hover:scale-105',
+        theme === 'light' ? 'bg-white text-black' : 'bg-black text-white',
+        theme === 'light' ? 'border-black' : 'border-white',
+        'mt-4 border-2 font-bold',
+      )}
       onClick={exampleThemeStorage.toggle}
       {...props}>
       {children}
-    </Button>
+    </button>
   );
 };
