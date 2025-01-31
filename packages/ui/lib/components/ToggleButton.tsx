f5e2ccc86c2614be1fd6b7abb1a1ac23adef05ef
Patryk Kuniczak <p.kuniczak@gmail.com>
2025-01-31 02:53:53 +0100

    fix: use ToggleButton over Button for all pages and move styles inside ToggleButton (#847)

--- packages/ui/lib/components/ToggleButton.tsx
+++ packages/ui/lib/components/ToggleButton.tsx
@@ -1,13 +1,25 @@
-import type { ButtonProps } from './Button';
-import { Button } from './Button';
 import { exampleThemeStorage } from '@extension/storage';
+import { useStorage } from '@extension/shared';
+import type { ComponentPropsWithoutRef } from 'react';
+import { cn } from '@/lib/utils';
 
-type ToggleButtonProps = ButtonProps;
+type ToggleButtonProps = ComponentPropsWithoutRef<'button'>;
 
 export const ToggleButton = ({ className, children, ...props }: ToggleButtonProps) => {
+  const theme = useStorage(exampleThemeStorage);
+
   return (
-    <Button className={className + 'font-bold mt-4'} onClick={exampleThemeStorage.toggle} {...props}>
+    <button
+      className={cn(
+        className,
+        'py-1 px-4 rounded shadow hover:scale-105',
+        theme === 'light' ? 'bg-white text-black' : 'bg-black text-white',
+        theme === 'light' ? 'border-black' : 'border-white',
+        'mt-4 border-2 font-bold',
+      )}
+      onClick={exampleThemeStorage.toggle}
+      {...props}>
       {children}
-    </Button>
+    </button>
   );
 };
