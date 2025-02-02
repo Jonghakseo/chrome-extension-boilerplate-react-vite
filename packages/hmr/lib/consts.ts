d878ef77fd8f4fd1a0063c3fcfc6a3c6a303ce71
PatrykKuniczak <p.kuniczak@gmail.com>
2025-02-02 12:48:14 +0100

    fix: change `SIGKILL` to `uncaughtException` for `TERMINATE_SIGNALS` for fix POSIX-compliant machines and handle errors during wss runtime

--- packages/hmr/lib/consts.ts
+++ packages/hmr/lib/consts.ts
@@ -5,4 +5,4 @@ export const DO_UPDATE = 'do_update';
 export const DONE_UPDATE = 'done_update';
 export const BUILD_COMPLETE = 'build_complete';
 
-export const TERMINATE_SIGNALS = ['SIGINT', 'SIGTERM', 'SIGHUP', 'SIGKILL'] as const;
+export const TERMINATE_SIGNALS = ['SIGINT', 'SIGTERM', 'SIGHUP', 'uncaughtException'] as const;
