52c2808f4391962cb138f0d7d1c94e9552ed9ffa
PatrykKuniczak <p.kuniczak@gmail.com>
2025-02-09 13:53:37 +0100

    feat: change all packages to be `module`

--- packages/ui/lib/components/ToggleButton.tsx
+++ packages/ui/lib/components/ToggleButton.tsx
@@ -1,7 +1,7 @@
 import { exampleThemeStorage } from '@extension/storage';
 import { useStorage } from '@extension/shared';
 import type { ComponentPropsWithoutRef } from 'react';
-import { cn } from '@/lib/utils';
+import { cn } from '@/lib/utils.js';
 
 type ToggleButtonProps = ComponentPropsWithoutRef<'button'>;
 
