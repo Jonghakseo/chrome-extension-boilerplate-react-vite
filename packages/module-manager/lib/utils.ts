75e0959e966708a6331be38836d6e9b35e0aac2c
PatrykKuniczak <p.kuniczak@gmail.com>
2025-03-17 16:51:09 +0100

    refactor: simplify selectFeatures func and create func for getting prompt from user(with error handling)

--- packages/module-manager/lib/utils.ts
+++ packages/module-manager/lib/utils.ts
@@ -1,29 +1,18 @@
 import { select } from '@inquirer/prompts';
 import { EXIT_PROMPT_ERROR } from './const.js';
-import type { ActionType, ChoiceType } from './types.ts';
+import type { InputConfigType } from './types.js';
 
-export const selectFeatures = async (action: ActionType, choices: ChoiceType[]): Promise<string> => {
-  if (!choices.length) {
-    console.log(`No features to ${action}`);
+export const promptSelection = async (inputConfig: InputConfigType) => {
+  if (!inputConfig.choices.length) {
+    console.log('No choices available');
     process.exit(0);
   }
 
-  const answer = await select({
-    message: `Choose the features you want to ${action}`,
-    loop: false,
-    choices,
-  }).catch(err => {
+  return select(inputConfig).catch(err => {
     if (err.name === EXIT_PROMPT_ERROR) {
       process.exit(0);
     } else {
       console.error(err.message);
     }
-  });
-
-  if (!answer) {
-    console.log(`No feature selected`);
-    process.exit(0);
-  }
-
-  return answer;
+  }) as Promise<string>;
 };
