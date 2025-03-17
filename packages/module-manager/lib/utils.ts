6abe93253b938e760f1bfa6a97d803c4fab5843a
PatrykKuniczak <p.kuniczak@gmail.com>
2025-03-17 08:12:40 +0100

    fix: selecting feature from CLI

--- packages/module-manager/lib/utils.ts
+++ packages/module-manager/lib/utils.ts
@@ -1,22 +1,22 @@
-import { checkbox } from '@inquirer/prompts';
+import { select } from '@inquirer/prompts';
 import type { ActionType, ChoiceType } from './types.ts';
 
-export async function selectFeatures(action: ActionType, choices: ChoiceType[]): Promise<string[]> {
+export const selectFeatures = async (action: ActionType, choices: ChoiceType[]): Promise<string> => {
   if (!choices.length) {
     console.log(`No features to ${action}`);
     process.exit(0);
   }
 
-  const answers = await checkbox({
+  const answer = await select({
     message: `Choose the features you want to ${action}`,
     loop: false,
     choices,
   });
 
-  if (!answers.length) {
+  if (!answer) {
     console.log(`No feature selected`);
     process.exit(0);
   }
 
-  return answers;
-}
+  return answer;
+};
