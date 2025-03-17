ad428d3e361cd0eb9983441f63bd8b891ee73c37
PatrykKuniczak <p.kuniczak@gmail.com>
2025-03-17 10:45:00 +0100

    refactor: move IModuleConfig inside types.ts

--- packages/module-manager/lib/types.ts
+++ packages/module-manager/lib/types.ts
@@ -1,5 +1,10 @@
+import type { ValueOf } from '@extension/shared';
 import type { DEFAULT_CHOICES } from './const.js';
 
 export type ModuleNameType = 'content' | 'background' | 'new-tab' | 'popup' | 'devtools' | 'side-panel' | 'options';
 export type ChoiceType = (typeof DEFAULT_CHOICES)[number];
 export type ActionType = 'recover' | 'delete';
+
+export interface IModuleConfig {
+  [key: string]: ValueOf<chrome.runtime.ManifestV3>;
+}
