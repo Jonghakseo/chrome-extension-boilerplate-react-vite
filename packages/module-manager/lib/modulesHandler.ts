5d3dc64cef06e4cc9fc9180cfd8cb997c020f84c
PatrykKuniczak <p.kuniczak@gmail.com>
2025-03-18 12:38:10 +0100

    fix: create removing content form Popup.tsx on content-runtime remove

--- packages/module-manager/lib/modulesHandler.ts
+++ packages/module-manager/lib/modulesHandler.ts
@@ -1,3 +1,4 @@
+import { readFileSync, writeFileSync } from 'node:fs';
 import { resolve } from 'node:path';
 import { rimraf } from 'rimraf';
 import { MODULE_CONFIG } from './const.js';
@@ -10,7 +11,10 @@ export const recoverModule = (
   pagesPath: string,
   archivePath: string,
 ) => {
-  Object.assign(manifestObject, MODULE_CONFIG[moduleName]);
+  if (moduleName !== 'content-runtime') {
+    Object.assign(manifestObject, MODULE_CONFIG[moduleName]);
+  }
+
   const zipFilePath = resolve(archivePath, `${moduleName}.zip`);
   upZipAndDelete(zipFilePath, resolve(pagesPath, moduleName));
   console.log(`Recovered: ${moduleName}`);
@@ -25,10 +29,22 @@ export const deleteModule = async (
   await zipFolder(resolve(pagesPath, moduleName), resolve(archivePath, `${moduleName}.zip`));
   void rimraf(resolve(pagesPath, moduleName));
   const jsName = `${moduleName}/index.iife.js`;
+  const popupPath = resolve(pagesPath, 'popup', 'src', 'Popup.tsx');
+  const popupContent = readFileSync(popupPath, 'utf-8');
 
   if (moduleName.startsWith('content')) {
+    if (moduleName === 'content-runtime') {
+      const updatedContent = popupContent
+        .replace(/const injectContentScript = async \(\) => {[\s\S]*?};(\r?\n)*/, '')
+        .replace(/<button[^>]*onClick=\{injectContentScript}[^>]*>[^<]*<\/button>(\r?\n)*/, '')
+        .replace(/const notificationOptions = {[\s\S]*?} as const;(\r?\n)*/, '');
+
+      writeFileSync(popupPath, updatedContent);
+      return;
+    }
     manifestObject.content_scripts = manifestObject.content_scripts?.filter(script => !script.js?.includes(jsName));
   } else {
+    // @ts-expect-error TS recognizing error
     Object.keys(MODULE_CONFIG[moduleName]).forEach(key => {
       if (manifestObject[key]) {
         delete manifestObject[key];
--- packages/module-manager/lib/types.ts
+++ packages/module-manager/lib/types.ts
@@ -1,7 +1,7 @@
 import type { select } from '@inquirer/prompts';
 import type { DEFAULT_CHOICES } from './const.js';
 
-export type ModuleNameType = Exclude<(typeof DEFAULT_CHOICES)[number]['value'], 'content-runtime'>;
+export type ModuleNameType = (typeof DEFAULT_CHOICES)[number]['value'];
 
 export type ChoiceType = (typeof DEFAULT_CHOICES)[number];
 export type InputConfigType = Parameters<typeof select>[0];
