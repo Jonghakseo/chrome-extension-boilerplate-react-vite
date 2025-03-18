5457a70600edf1f964559595974d13bcc613fd05
Jonghakseo <unqocn@gmail.com>
2025-03-18 19:35:44 +0900

    feat: add test

--- tests/e2e/specs/page-content-ui.test.ts
+++ tests/e2e/specs/page-content-ui.test.ts
@@ -1,8 +1,21 @@
 describe('Content UI Injection', () => {
-  it('should locate the injected content UI div', async () => {
+  it('should locate the injected content UI div on example.com and not find google div', async () => {
     await browser.url('https://www.example.com');
 
-    const contentDiv = await $('#chrome-extension-boilerplate-react-vite-content-view-root').getElement();
+    const contentDiv = await $('#CEB-extension-all').getElement();
+    await expect(contentDiv).toBeDisplayed();
+
+    const contentDivOnGoogle = await $('#CEB-extension-google').getElement();
+    await expect(contentDivOnGoogle).not.toBeDisplayed();
+  });
+
+  it('should locate the injected content UI div on google.com and all div', async () => {
+    await browser.url('https://www.google.com');
+
+    const contentDivOnGoogle = await $('#CEB-extension-google').getElement();
+    await expect(contentDivOnGoogle).toBeDisplayed();
+
+    const contentDiv = await $('#CEB-extension-all').getElement();
     await expect(contentDiv).toBeDisplayed();
   });
 });
