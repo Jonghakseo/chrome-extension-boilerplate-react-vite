76619bd1b31d4fa36596a35be17b16a451bad277
PatrykKuniczak <p.kuniczak@gmail.com>
2025-03-18 16:08:00 +0100

    fix: save popup on content-runtime removing

--- packages/module-manager/lib/modulesHandler.ts
+++ packages/module-manager/lib/modulesHandler.ts
@@ -29,17 +29,20 @@ export const deleteModule = async (
   await zipFolder(resolve(pagesPath, moduleName), resolve(archivePath, `${moduleName}.zip`));
   void rimraf(resolve(pagesPath, moduleName));
   const jsName = `${moduleName}/index.iife.js`;
-  const popupPath = resolve(pagesPath, 'popup', 'src', 'Popup.tsx');
-  const popupContent = readFileSync(popupPath, 'utf-8');
+
+  const popupTsxPath = resolve(pagesPath, 'popup', 'src', 'Popup.tsx');
+  const popupContent = readFileSync(popupTsxPath, 'utf-8');
 
   if (moduleName.startsWith('content')) {
     if (moduleName === 'content-runtime') {
+      await zipFolder(resolve(pagesPath, 'popup'), resolve(archivePath, 'popup.zip'));
+
       const updatedContent = popupContent
         .replace(/const injectContentScript = async \(\) => {[\s\S]*?};(\r?\n)*/, '')
         .replace(/<button[^>]*onClick=\{injectContentScript}[^>]*>[^<]*<\/button>(\r?\n)*/, '')
         .replace(/const notificationOptions = {[\s\S]*?} as const;(\r?\n)*/, '');
 
-      writeFileSync(popupPath, updatedContent);
+      writeFileSync(popupTsxPath, updatedContent);
       return;
     }
     manifestObject.content_scripts = manifestObject.content_scripts?.filter(script => !script.js?.includes(jsName));
