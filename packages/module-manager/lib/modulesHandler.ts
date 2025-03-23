bbc54c0378f939e1fa62be51eeb15e86545f4433
PatrykKuniczak <p.kuniczak@gmail.com>
2025-03-23 11:26:54 +0100

    fix: add popup exists checking on content-runtime remove

--- packages/module-manager/lib/modulesHandler.ts
+++ packages/module-manager/lib/modulesHandler.ts
@@ -1,6 +1,7 @@
 import { MODULE_CONFIG } from './const.js';
 import { removeContentRuntimeReferencesFromPopup, zipAndDeleteModuleWithTest } from './utils.js';
 import { unZipAndDelete } from './zipUtils.js';
+import { existsSync } from 'node:fs';
 import { resolve } from 'node:path';
 import type { ModuleNameType } from './types.ts';
 import type { ManifestType } from '@extension/shared';
@@ -9,6 +10,9 @@ const pagesPath = resolve(import.meta.dirname, '..', '..', '..', 'pages');
 const testsPath = resolve(pagesPath, '..', 'tests', 'e2e', 'specs');
 
 export const recoverModule = (manifestObject: ManifestType, moduleName: ModuleNameType, archivePath: string) => {
+  const zipFilePath = resolve(archivePath, `${moduleName}.zip`);
+  const zipTestFilePath = resolve(archivePath, `${moduleName}.test.zip`);
+
   if (moduleName !== 'content-runtime' && moduleName !== 'devtools-panel') {
     if (moduleName.startsWith('content'))
       // @ts-expect-error recognizing .startsWith() error
@@ -18,9 +22,6 @@ export const recoverModule = (manifestObject: ManifestType, moduleName: ModuleNa
     }
   }
 
-  const zipFilePath = resolve(archivePath, `${moduleName}.zip`);
-  const zipTestFilePath = resolve(archivePath, `${moduleName}.test.zip`);
-
   unZipAndDelete(zipFilePath, pagesPath.at(-1) as NonNullable<string>);
   unZipAndDelete(zipTestFilePath, testsPath.at(-1) as NonNullable<string>);
   console.log(`Recovered: ${moduleName}`);
@@ -28,13 +29,14 @@ export const recoverModule = (manifestObject: ManifestType, moduleName: ModuleNa
 
 export const deleteModule = async (manifestObject: ManifestType, moduleName: ModuleNameType, archivePath: string) => {
   await zipAndDeleteModuleWithTest(moduleName, pagesPath, archivePath, testsPath);
+  const popupZipExists = existsSync(resolve(archivePath, 'popup.zip'));
+  const outputFileName = `${moduleName}/index.iife.js`;
 
   if (moduleName.startsWith('content')) {
-    if (moduleName === 'content-runtime') {
+    if (moduleName === 'content-runtime' && !popupZipExists) {
       await removeContentRuntimeReferencesFromPopup(pagesPath, archivePath);
       return;
     }
-    const outputFileName = `${moduleName}/index.iife.js`;
 
     manifestObject.content_scripts = manifestObject.content_scripts?.filter(
       script => !script.js?.includes(outputFileName),
