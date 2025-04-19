2a25394a3271a6b54a8b8bf24f1ce7ba70752eaa
PatrykKuniczak <p.kuniczak@gmail.com>
2025-04-19 22:22:14 +0200

    chore: move color logger from `dev-utils` to `shared` (rename it also)

--- packages/dev-utils/lib/index.ts
+++ packages/dev-utils/lib/index.ts
@@ -1,3 +1,2 @@
-export * from './logger.js';
 export * from './stream-file-to-zip.js';
 export * from './manifest-parser/index.js';
--- packages/shared/lib/utils/types.ts
+++ packages/shared/lib/utils/types.ts
@@ -1,2 +1,6 @@
+import type { COLORS } from './const.js';
+
+export type ColorType = 'success' | 'info' | 'error' | 'warning' | keyof typeof COLORS;
+
 export type * from 'type-fest';
 export type ManifestType = chrome.runtime.ManifestV3;
