5777f0df510788a4f25ef0ca3739eda4ce8d4c75
PatrykKuniczak <p.kuniczak@gmail.com>
2025-04-21 15:04:45 +0200

    feat: add 0.5 s delay for HMR reload

--- packages/vite-config/lib/withPageConfig.ts
+++ packages/vite-config/lib/withPageConfig.ts
@@ -9,6 +9,7 @@ import type { UserConfig } from 'vite';
 export const watchOption = IS_DEV
   ? {
       exclude: [/\/pages\/content-ui\/dist\/.*\.(css)$/],
+      delay: 500,
       chokidar: {
         awaitWriteFinish: true,
         ignored: [/\/packages\/.*\.(ts|tsx|map)$/, /\/pages\/content-ui\/dist\/.*/],
