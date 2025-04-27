957a6ed3a1b8a59470a463a8853b37ebba66ba76
johnny doe <johnny.doe64@outlook.fr>
2025-04-27 16:55:56 +0200

    handle the tailwind dark mode in side-panel page too

--- pages/side-panel/src/SidePanel.tsx
+++ pages/side-panel/src/SidePanel.tsx
@@ -1,21 +1,27 @@
 import '@src/SidePanel.css';
 import { t } from '@extension/i18n';
-import { useStorage, withErrorBoundary, withSuspense } from '@extension/shared';
+import { withErrorBoundary, withSuspense } from '@extension/shared';
 import { exampleThemeStorage } from '@extension/storage';
-import { cn, ErrorDisplay, LoadingSpinner, ToggleButton } from '@extension/ui';
+import { ErrorDisplay, LoadingSpinner, ToggleButton } from '@extension/ui';
 
 const SidePanel = () => {
-  const { isLight } = useStorage(exampleThemeStorage);
-  const logo = isLight ? 'side-panel/logo_vertical.svg' : 'side-panel/logo_vertical_dark.svg';
-
   const goGithubSite = () =>
     chrome.tabs.create({ url: 'https://github.com/Jonghakseo/chrome-extension-boilerplate-react-vite' });
 
   return (
-    <div className={cn('App', isLight ? 'bg-slate-50' : 'bg-gray-800')}>
-      <header className={cn('App-header', isLight ? 'text-gray-900' : 'text-gray-100')}>
+    <div className="App bg-slate-50 dark:bg-gray-800">
+      <header className="App-header text-gray-900 dark:text-gray-100">
         <button onClick={goGithubSite}>
-          <img src={chrome.runtime.getURL(logo)} className="App-logo" alt="logo" />
+          <img
+            src={chrome.runtime.getURL('side-panel/logo_vertical.svg')}
+            className="App-logo dark:hidden"
+            alt="logo"
+          />
+          <img
+            src={chrome.runtime.getURL('side-panel/logo_vertical_dark.svg')}
+            className="App-logo hidden dark:block"
+            alt="logo"
+          />
         </button>
         <p>
           Edit <code>pages/side-panel/src/SidePanel.tsx</code>
