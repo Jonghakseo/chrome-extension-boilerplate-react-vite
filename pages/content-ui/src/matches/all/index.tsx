fa5da9c4cd8a82b70b1a2d97869cf4dd377add16
PatrykKuniczak <p.kuniczak@gmail.com>
2025-04-27 16:53:41 +0200

    feat: define module for ``?inline` imports for `content-ui`

--- pages/content-ui/src/matches/all/index.tsx
+++ pages/content-ui/src/matches/all/index.tsx
@@ -1,4 +1,3 @@
-// @ts-expect-error Because file doesn't exist before build
 import inlineCss from '../../../dist/all/index.css?inline';
 import { initAppWithShadow } from '@extension/shared';
 import App from '@src/matches/all/App';
