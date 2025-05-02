82c4948986c4ab111b834b5fd311c7e90aa43f8d
Patryk Kuniczak <p.kuniczak@gmail.com>
2025-05-02 21:59:55 +0200

    fix: adjust env package to look like other packages (#971)

--- packages/env/lib/index.ts
+++ packages/env/lib/index.ts
@@ -1,10 +1,2 @@
-import { config } from '@dotenvx/dotenvx';
-
-export const baseEnv =
-  config({
-    path: `${import.meta.dirname}/../../../../.env`,
-  }).parsed ?? {};
-
-export const dynamicEnvValues = {
-  CEB_NODE_ENV: baseEnv.CEB_DEV === 'true' ? 'development' : 'production',
-} as const;
+export * from './config.js';
+export * from './const.js';
