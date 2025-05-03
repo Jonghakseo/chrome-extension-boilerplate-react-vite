63cd452723eb0ea048c074a369efc1f51dc67df6
Patryk Kuniczak <p.kuniczak@gmail.com>
2025-05-03 22:18:28 +0200

    feat: use `Type` suffix for types and `I` prefix for interfaces (#975)

--- packages/shared/lib/utils/helpers.ts
+++ packages/shared/lib/utils/helpers.ts
@@ -1,8 +1,8 @@
-import type { TExcludeValuesFromBaseArray } from './types.js';
+import type { ExcludeValuesFromBaseArrayType } from './types.js';
 
 export const excludeValuesFromBaseArray = <B extends string[], E extends (string | number)[]>(
   baseArray: B,
   excludeArray: E,
-) => baseArray.filter(value => !excludeArray.includes(value)) as TExcludeValuesFromBaseArray<B, E>;
+) => baseArray.filter(value => !excludeArray.includes(value)) as ExcludeValuesFromBaseArrayType<B, E>;
 
 export const sleep = async (time: number) => new Promise(r => setTimeout(r, time));
