fa8aee6a0d10f8f2266a913fb0f499f9313ecb37
PatrykKuniczak <p.kuniczak@gmail.com>
2025-05-10 15:27:24 +0200

    feat: remove tailwind-config package and move all configs inside each page `package.json`

--- packages/tailwindcss-config/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from 'tailwindcss';
-
-export default {
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} as Omit<Config, 'content'>;
--- pages/new-tab/tailwind.config.ts
+++
@@ -1,5 +0,0 @@
-import { withUI } from '@extension/ui';
-
-export default withUI({
-  content: ['index.html', 'src/**/*.tsx'],
-});
