63615acb99c156e1e14fd81e7bc4d37adb2b1f00
PatrykKuniczak <p.kuniczak@gmail.com>
2025-05-18 13:25:46 +0200

    feat: remove with-ui.ts

--- packages/ui/lib/with-ui.ts
+++
@@ -1,7 +0,0 @@
-import deepmerge from 'deepmerge';
-import type { Config } from 'tailwindcss';
-
-export const withUI = (tailwindConfig: Config): Config =>
-  deepmerge(tailwindConfig, {
-    content: ['../../packages/ui/lib/**/*.tsx'],
-  });
